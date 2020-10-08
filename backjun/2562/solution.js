6228e5f659ed38bdd8608e2ed8c0e38bcdbc740f
dreammarker <minjea456@naver.com>
2020-10-08 15:04:13 +0900

    알고리즘 문제 풀기

---
+++ backjun/2562/solution.js
@@ -0,0 +1,21 @@
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+rl.on('line', function (line) {
+  input.push(parseInt(line));
+}).on('close', function () {
+    let max = input[0];
+    for(let i=0;i<input.length;i++){
+        if(max < input[i]){
+            max = input[i];
+        }
+    }
+    console.log(max+"\n"+(input.indexOf(max)+1));
+    process.exit();
+});
+
